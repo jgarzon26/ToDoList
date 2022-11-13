4ca0080fdd3a0be8e3e37f663688743442d11fab
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2022-11-13 14:33:59 +0800

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,48 @@
 import 'package:flutter/material.dart';
+
+main() => runApp(
+  MaterialApp(
+    home: Home(),
+  )
+);
+
+class Home extends StatefulWidget{
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+
+  @override
+  Widget build(BuildContext context){
+    return(MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("To Do List"),
+            actions: [
+              IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.delete,
+                  )
+              ),
+            ],
+          ),
+          body: ListView(
+            children: [
+
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: null,
+            child: Icon(
+              Icons.add,
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
